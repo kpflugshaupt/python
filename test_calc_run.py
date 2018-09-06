3add185a000aab9ceed7c0d70c81e3e027abd17c
Kati & Kaspar Pflugshaupt <pflugshaupt@mails.ch>
2018-09-06 13:52:44 +0200

    reformat

--- test_calc_run.py
+++ test_calc_run.py
@@ -1,8 +1,8 @@
 import calc_run.py
 
 def test_rd():
-  assert calc_run.rd(3.0) == 3.0
-  assert calc_run.rd(3.14) == 3.1
-  assert calc_run.rd(3.15) == 3.2
+  assert calc_run.rd(3.0)   == 3.0
+  assert calc_run.rd(3.14)  == 3.1
+  assert calc_run.rd(3.15)  == 3.2
   assert calc_run.rd(-1.15) == -1.2
  
